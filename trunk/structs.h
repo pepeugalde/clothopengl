3354b9a66730fd6171b5ad4c0734f0001e6cf534
pepe.ugalde <pepe.ugalde@56c8a247-4518-1e04-c985-807d4f1928a3>
2010-05-01 01:52:08 +0000

    Mas documentado, menu click der mejorado, etc

--- trunk/structs.h
+++ trunk/structs.h
@@ -1,12 +1,17 @@
+/*Punto, vector, vertice, como lo quieran ver
+contiene x, y, z */
 typedef struct Vec3{
     float f[3];
 } Vec3;
 
+/*vertice del cuerpo (hombro, cadera, codo, etc)
+contiene un punto y bool para no rotarlo mas de una vez*/
 typedef struct bodyvertex{
     Vec3 v;
     bool hasmoved;
 } bodyvertex;
 
+/*si*/
 typedef struct capsule{
     bodyvertex *bv1;
     bodyvertex *bv2;
@@ -15,6 +20,8 @@ typedef struct capsule{
     char name[16];
 } capsule;
 
+/*parte del cuerpo que contiene 2 vertices
+igual y se puede simplificar hasta 1... no se*/
 typedef struct treenode{
     capsule *cap;
     int id;
@@ -36,13 +43,13 @@ typedef struct particle{
 	Vec3 accumulated_normal; // an accumulated normal (i.e. non normalized), used for OpenGL soft shading
 } particle;
 
-
-typedef struct constraint{
+/*resorte entre 2 particulas*/
+typedef struct spring{
 	float rest_distance; // the length between particle p1 and p2 in rest configuration
 	particle *p1, *p2; // the two particles that are connected through this constraint
-} constraint;
+} spring;
 
-
+/*no se si usar esto*/
 typedef struct cloth{
 	int num_particles_width; // number of particles in "width" direction
 	int num_particles_height; // number of particles in "height" direction
